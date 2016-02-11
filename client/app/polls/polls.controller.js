554a15559191ed5943ea859e7abb1c31c9a41a81
Sebastian Bejga <sebastian@bejga.de>
2016-02-11 16:29:42 +0100

    client: get Polls from server and display as list

--- client/app/polls/polls.controller.js
+++ client/app/polls/polls.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('ngvoteApp')
-  .controller('PollsCtrl', function ($scope) {
+  .controller('PollsCtrl', function ($scope, $http, socket) {
     $scope.polls = [];
+
+    $scope.getPolls = function () {
+      $http.get('/api/polls').success(function(polls) {
+        $scope.polls = polls;
+        socket.syncUpdates('polls', $scope.polls);
+      });
+    };
+
+    //Load on Controller Init
+    $scope.getPolls();
   });
