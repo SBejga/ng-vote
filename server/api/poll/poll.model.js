78b53ce94419003f1f01939d858b9fba6323e33a
Sebastian Bejga <sebastian@bejga.de>
2016-02-11 16:29:22 +0100

    server: update Poll model to store votes etc

--- server/api/poll/poll.model.js
+++ server/api/poll/poll.model.js
@@ -3,10 +3,23 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+// Subdocument schema for votes
+var voteSchema = new mongoose.Schema({
+  fingerprint: String,
+  user: String
+});
+
+// Subdocument schema for poll choices
+var choiceSchema = new mongoose.Schema({
+  text: String,
+  votes: [voteSchema]
+});
+
 var PollSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  question: String,
+  choices: [choiceSchema],
+  active: {type: Boolean, default: false},
+  closed: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('Poll', PollSchema);
